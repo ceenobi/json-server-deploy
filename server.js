b106d23a76fa3bc2e7aaf4701e42f0a5399e2226
ceenobi <cobimbachu@gmail.com>
2022-12-17 02:46:26 +0100

    fixes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server, ./node_modules/json-server-auth')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
